5f231b3636db9c5efe978ba9926038b72fc9ded3
Alexander Spivak <spivak95alexander@gmail.com>
2021-09-21 18:17:13 +0300

    Создан Spring Boot проект

---
+++ src/main/java/ru/moscow/flyin/FlyInApplication.java
@@ -0,0 +1,13 @@
+package ru.moscow.flyin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyInApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/moscow/flyin/FlyInApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.moscow.flyin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyInApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
